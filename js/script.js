1623933bd1b5656819b8378f0129249ff2bb2fbf
filippo <schianchi.filippo.93@gmail.com>
2019-10-16 14:32:11 +0200

    esercizio completato

--- js/script.js
+++ js/script.js
@@ -2,5 +2,5 @@ var nome = prompt("Quale e' il tuo nome?");
 var cognome = prompt("Quale e' il tuo cognome?");
 var colore = prompt("Quale e' il tuo colore preferito?");
 
-document.getElementById("testo1").innerHTML="La tua password e':";
+document.getElementById("testo1").innerHTML="Ciao " + nome + " " + cognome + "! La tua password generata è: ";
 document.getElementById("testo2").innerHTML= nome + cognome + colore + "19";
